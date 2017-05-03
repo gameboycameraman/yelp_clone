4232d92a55be6c55c5b4fdc3e04748acfe108243
kittysquee <kittysquee@gmail.com>
2017-05-03 11:23:01 +0100

    Finishes 12

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -9,7 +9,6 @@ feature 'reviewing' do
     fill_in 'Thoughts', with: 'Amazing'
     select '5', from: 'Rating'
     click_button 'Leave Review'
-    expect(current_path).to eq restaurants_path
     expect(page).to have_content 'Amazing'
   end
 end
